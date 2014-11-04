66a3964c9871b8225ccacb2d17a14f13285cf2d7
mdeboer1 <mdeboer1@L11108042787.wctc.msft>
2014-11-03 18:32:34 -0600

    Added some corrections to Lab3 (not complete)

--- src/lab3/NameNotProvidedException.java
+++ src/lab3/NameNotProvidedException.java
@@ -10,17 +10,29 @@ package lab3;
  * @author mdeboer1
  */
 public class NameNotProvidedException extends IllegalArgumentException {
-    private static String message = "You must provide a first and last name!";
+    private static final String MESSAGE = "You must provide a first and last name!";
 
     public NameNotProvidedException() {
-        super(message);
+        super(MESSAGE);
     }
 
     public NameNotProvidedException(String s) {
-        super(message);
+        super(MESSAGE);
     }
     
-    public static void setMessage(String message){
-        NameNotProvidedException.message = message;
-    }    
+    //Add other constructors to match the IllegalArgumentClass, all must be 
+    //provided for!  Missing one with string and throwable and one with just
+    // throwable
+    
+    public NameNotProvidedException(String s, Throwable cause) {
+        super(MESSAGE, cause);
+    }
+
+    public NameNotProvidedException(Throwable cause) {
+        super(cause);
+    }
+    
+//    public static void setMessage(String MESSAGE){
+//        NameNotProvidedException.MESSAGE = MESSAGE;
+//    }    
 }
